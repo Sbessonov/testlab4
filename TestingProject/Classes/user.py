136d7ee72584b7ff39ade2d407a8a6c841a61a39
Sbessonov <52399828+Sbessonov@users.noreply.github.com>
2019-12-27 23:14:34 +0300

    Class_User

---
+++ TestingProject/Classes/user.py
@@ -0,0 +1,11 @@
+class User:
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.first_name = None
+        self.last_name = None
+        self.birth_date = None
+        self.list_friends = None
+        self.zodiac = None
+        self.domain = None
+        self.city = None
